67081cd3e236db23832ab6573c6fa6661f70beac
Andrea Mendez <andbermen@alu.edu.gva.es>
2025-01-30 08:53:59 +0100

    Arreglos en el header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ function Header(props) {
   return (
     <div>
       <h1 className="text-center py-4 fs-4 fw-bold m-0 text-white bg-title">{title}</h1>
-      <p className='position-absolute top-0 end-0 me-4 mt-4 fw-bold fs-5 text-white'><i className="bi bi-person-circle pe-2"></i>Usuari Admin</p>
+      <p className='position-absolute top-0 end-0 me-4 m-customized mt-xl-4 fw-bold fs-5 text-white'><i className="bi bi-person-circle pe-2"></i>Usuari Admin</p>
     </div>
 
   )
