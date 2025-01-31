dc1e44f128cde92d9ae793218831cd82f2f3f205
looreenz <lorenzo.cremonese10@gmail.com>
2025-01-31 12:47:29 +0100

    Integrando estilos Bootstrap

--- src/components/Enviament/OrderPickingShipping.jsx
+++ src/components/Enviament/OrderPickingShipping.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import {
   url,
   postData,
-  getData,
+  // getData,
   // deleteData,
   // updateId,
 } from "../../apiAccess/crud";
@@ -27,64 +27,64 @@ function OrderPickingShipping() {
   const [orderSelected, setOrderSelected] = useState([]);
 
   useEffect(() => {
-    const apiUrl = import.meta.env.VITE_API_URL;
-    const token = localStorage.getItem("token");
-    
+    // const apiUrl = import.meta.env.VITE_API_URL;
+    const apiUrl = url;
+    // const token = localStorage.getItem("token");
 
     const fetchData = async () => {
       axios
-      .get(`${apiUrl}orderpickingshipping`, {
-        headers: { "auth-token": token },
-      })
-      .then((res) => {
-        setOrderPickingShipping(res.data);
-      })
-      .catch((error) => console.log(error));
+        .get(`${apiUrl}OrderPickingshipping`, {
+          // headers: { "auth-token": token },
+        })
+        .then((res) => {
+          setOrderPickingShipping(res.data);
+        })
+        .catch((error) => console.log(error));
 
       axios
-      .get(`${apiUrl}ordershipping`, {
-        headers: { "auth-token": token },
-      })
-      .then((res) => {
-        setOrderShipping(res.data);
-      })
-      .catch((error) => console.log(error));
+        .get(`${apiUrl}OrderShipping`, {
+          // headers: { "auth-token": token },
+        })
+        .then((res) => {
+          setOrderShipping(res.data);
+        })
+        .catch((error) => console.log(error));
 
       axios
-      .get(`${apiUrl}orderlineshipping`, {
-        headers: { "auth-token": token },
-      })
-      .then((res) => {
-        setOrderLineShipping(res.data);
-      })
-      .catch((error) => console.log(error));
+        .get(`${apiUrl}OrderLineShipping`, {
+          // headers: { "auth-token": token },
+        })
+        .then((res) => {
+          setOrderLineShipping(res.data);
+        })
+        .catch((error) => console.log(error));
 
       axios
-      .get(`${apiUrl}product`, {
-        headers: { "auth-token": token },
-      })
-      .then((res) => {
-        setProducts(res.data);
-      })
-      .catch((error) => console.log(error));
+        .get(`${apiUrl}Product`, {
+          // headers: { "auth-token": token },
+        })
+        .then((res) => {
+          setProducts(res.data);
+        })
+        .catch((error) => console.log(error));
 
       axios
-      .get(`${apiUrl}space`, {
-        headers: { "auth-token": token },
-      })
-      .then((res) => {
-        setSpaces(res.data);
-      })
-      .catch((error) => console.log(error));
+        .get(`${apiUrl}Space`, {
+          // headers: { "auth-token": token },
+        })
+        .then((res) => {
+          setSpaces(res.data);
+        })
+        .catch((error) => console.log(error));
 
       axios
-      .get(`${apiUrl}users`, {
-        headers: { "auth-token": token },
-      })
-      .then((res) => {
-        setUsers(res.data);
-      })
-      .catch((error) => console.log(error));
+        .get(`${apiUrl}User`, {
+          // headers: { "auth-token": token },
+        })
+        .then((res) => {
+          setUsers(res.data);
+        })
+        .catch((error) => console.log(error));
 
       setCurrentUser(localStorage.getItem("currentuser"));
 
@@ -96,7 +96,7 @@ function OrderPickingShipping() {
       const tempPickings = [];
       orderPendent.map((order) => {
         //recorrer line reception de cada orden reception
-        const lines = orderLine.filter(
+        const lines = orderLineShipping.filter(
           (line) => line.shipping_order_id === order.id
         );
         //obtindre product.name, product.quantitat, product.space
@@ -132,7 +132,7 @@ function OrderPickingShipping() {
       });
     };
     fetchData();
-  }, []);
+  });
 
   const canviEstatModal = () => {
     setShowModal(!showModal);
@@ -186,16 +186,250 @@ function OrderPickingShipping() {
 
   return (
     <>
+      <div>
+        <h1 className="text-center py-4 fs-4 fw-bold m-0 text-white bg-title">
+          Llistat Order Picking Shipping
+        </h1>
+        <p className="position-absolute top-0 end-0 me-4 m-customized mt-xl-4 fw-bold fs-5 text-white">
+          <i className="bi bi-person-circle pe-2"></i>
+          Usuari Admin
+        </p>
+        <div className="container-fluid">
+          <div className="row bg-grey pt-3 px-2">
+            <div className="col-12 col-md-6 col-xl-4">
+              <div className="mb-3 text-light-blue">
+                <label htmlFor="nom" className="form-label">
+                  Nom
+                </label>
+                <input type="email" className="form-control" id="nom" />
+              </div>
+            </div>
+            <div className="col-12 col-md-6 col-xl-4">
+              <div className="mb-3 text-light-blue">
+                <label htmlFor="dni-nif" className="form-label">
+                  DNI/NIF
+                </label>
+                <input type="email" className="form-control" id="dni-nif" />
+              </div>
+            </div>
+            <div className="col-12 col-md-6 col-xl-4">
+              <div className="mb-3 text-light-blue">
+                <label htmlFor="telefon" className="form-label">
+                  Telèfon
+                </label>
+                <input type="email" className="form-control" id="telefon" />
+              </div>
+            </div>
+            <div className="col-12 col-md-6 col-xl-4">
+              <div className="mb-3 text-light-blue">
+                <label htmlFor="correo" className="form-label">
+                  Correu
+                </label>
+                <input type="email" className="form-control" id="correo" />
+              </div>
+            </div>
+            <div className="col-xl-4"></div>
+            <div className="col-xl-4"></div>
+          </div>
+          <div className="row bg-grey pb-3">
+            <div className="col-xl-4"></div>
+            <div className="col-xl-4"></div>
+            <div className="col-12 col-xl-4 text-end">
+              <button className="btn btn-secondary me-2 ps-2 text-white">
+                <i className="bi bi-trash px-1 text-white"></i>Netejar
+              </button>
+              <button className="btn btn-primary me-2 ps-2 orange-button text-white">
+                <i className="bi bi-funnel px-1 text-white"></i>Filtrar
+              </button>
+            </div>
+          </div>
+          <div className="row d-flex mx-0 bg-secondary mt-3 rounded-top">
+            <div className="col-12 order-1 pb-2 col-md-6 order-md-0 col-xl-4 d-flex">
+              <div className="d-flex rounded border mt-2 flex-grow-1 flex-xl-grow-0">
+                <div className="form-floating bg-white">
+                  <select
+                    className="form-select"
+                    id="floatingSelect"
+                    aria-label="Seleccione una opción"
+                  >
+                    <option selected>Tria una opció</option>
+                    <option value="1">Eliminar</option>
+                  </select>
+                  <label htmlFor="floatingSelect">Accions en lot</label>
+                </div>
+                <button
+                  className="btn rounded-0 rounded-end-2 orange-button text-white px-2 flex-grow-1 flex-xl-grow-0"
+                  type="button"
+                >
+                  <i className="bi bi-check-circle text-white px-1"></i>Aplicar
+                </button>
+              </div>
+            </div>
+            <div className="d-none d-xl-block col-xl-4 order-xl-1"></div>
+            <div className="col-12 order-0 col-md-6 order-md-1 col-xl-4 oder-xl-2">
+              <div className="d-flex h-100 justify-content-xl-end">
+                <button
+                  onClick={() => {
+                    crearOrderPickingShipping();
+                  }}
+                  type="button"
+                  className="btn btn-dark border-white text-white mt-2 my-md-2 flex-grow-1 flex-xl-grow-0"
+                >
+                  <i className="bi bi-plus-circle text-white pe-1"></i>Crear
+                </button>
+                <button
+                  onClick={() => {
+                    setTabla("ListarOrder");
+                  }}
+                  type="button"
+                  className="btn btn-dark ms-2 border-white text-white mt-2 my-md-2 flex-grow-1 flex-xl-grow-0"
+                >
+                  Llistar Orders Picking
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-12">
+              <div>
+                {tabla === "CrearOrder" ? (
+                  <>
+                    <table className="table table-striped text-center">
+                      <thead className="table-active border-bottom border-dark-subtle">
+                        <tr>
+                          <th scope="col">
+                            <input
+                              className="form-check-input"
+                              type="checkbox"
+                              name=""
+                              id=""
+                            />
+                          </th>
+                          <th scope="col">ID Order</th>
+                          <th scope="col">Producte</th>
+                          <th scope="col">Quantitat</th>
+                          <th scope="col">
+                            Magatzem / Carrer / Estantería / Espai
+                          </th>
+                        </tr>
+                      </thead>
+
+                      <tbody>
+                        {temporalPickings.map((temporalPicking) => {
+                          const product = products.find(
+                            (p) => p.id === temporalPicking.product_id
+                          );
+                          return (
+                            <tr key={temporalPicking.order_line_reception_id}>
+                              <td scope="row" data-cell="Seleccionar">
+                                <input
+                                  className="form-check-input"
+                                  type="checkbox"
+                                  value={
+                                    temporalPicking.order_line_reception_id
+                                  }
+                                />
+                              </td>
+                              <td>{temporalPicking.order_reception_id}</td>
+                              <td>{product.name}</td>
+                              <td>{temporalPicking.quantity}</td>
+                              <td>
+                                {temporalPicking.storage_id} /{" "}
+                                {temporalPicking.street_id} /{" "}
+                                {temporalPicking.selft_id} /{" "}
+                                {temporalPicking.space_id}
+                              </td>
+                            </tr>
+                          );
+                        })}
+                      </tbody>
+                    </table>
+                    <Modal show={showModal} onHide={canviEstatModal}>
+                      <Modal.Header closeButton>
+                        <Modal.Title>{tipoModal}</Modal.Title>
+                      </Modal.Header>
+
+                      <Modal.Body>
+                        {tipoModal === "Alta" && (
+                          <>
+                            <label htmlFor="operari"></label>
+                            <select name="operari" id="operari">
+                              <option
+                                value={currentuser ? currentuser : ""}
+                                selected
+                                disabled
+                              >
+                                Operari Actual
+                              </option>
+                              {users.map((user) => {
+                                return (
+                                  <option key={user.id} value={user.id}>
+                                    {user.name}
+                                  </option>
+                                );
+                              })}
+                            </select>
+
+                            <Table striped bordered hover>
+                              <thead>
+                                <tr>
+                                  <th>Producte</th>
+                                  <th>Quantitat</th>
+                                  <th>
+                                    Magatzem / Carrer / Estantería / Espai
+                                  </th>
+                                </tr>
+                              </thead>
+
+                              <tbody>
+                                {orderSelected.map((order) => {
+                                  const lines = orderLineShipping.find(
+                                    (line) => line.id === order
+                                  );
+                                  const product = products.find(
+                                    (p) => p.id === lines.product_id
+                                  );
+                                  const space = spaces.find(
+                                    (space) =>
+                                      space.product_id === lines.product_id
+                                  );
+                                  return (
+                                    <tr key={order}>
+                                      <td>{product.name}</td>
+                                      <td>{lines.quantity}</td>
+                                      <td>
+                                        {space.storage_id} / {space.street_id} /{" "}
+                                        {space.selft_id} / {space.id}
+                                      </td>
+                                    </tr>
+                                  );
+                                })}
+                              </tbody>
+                            </Table>
+                            <Button
+                              variant="success"
+                              onClick={() => {
+                                canviEstatModal();
+                                aceptarOrderPickingShipping();
+                              }}
+                            >
+                              Aceptar
+                            </Button>
+                          </>
+                        )}
+                      </Modal.Body>
+                    </Modal>
+                  </>
+                ) : (
+                  <></>
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
       {tabla === "CrearOrder" ? (
         <>
-          <div>
-            <h1 class="text-center py-4 fs-4 fw-bold m-0 text-white bg-title">Llistat Order Shipping</h1>
-            <p class="position-absolute top-0 end-0 me-4 m-customized mt-xl-4 fw-bold fs-5 text-white">
-              <i class="bi bi-person-circle pe-2"></i>
-              Usuari Admin
-            </p>
-          </div>
-
           <Button
             variant="success"
             onClick={() => {
