6880492b1650fad8af5e9c8428de0fc5db83e09f
MRoig99 <miguelangelroigancio@gmail.com>
2025-01-31 12:07:26 +0100

    tabla bootstrap

--- src/components/Enviament/OrdresEnviament.jsx
+++ src/components/Enviament/OrdresEnviament.jsx
@@ -1,6 +1,6 @@
-import {useState, useEffect} from 'react'
-import {url, postData, getData, deleteData, updateId}  from '../../apiAccess/crud'
-import {Formik, Form, Field} from 'formik'
+import { useState, useEffect } from 'react'
+import { url, postData, getData, deleteData, updateId } from '../../apiAccess/crud'
+import { Formik, Form, Field } from 'formik'
 import * as yup from 'yup'
 import { Button, Modal } from 'react-bootstrap';
 import Header from '../Header'
@@ -27,78 +27,78 @@ function OrdresEnviament() {
   const [status, setStatus] = useState([])
   const [showModal, setShowModal] = useState(false)
   const [tipoModal, setTipoModal] = useState('Crear')
-  const [valorsInicials, setValorsInicials] = useState({client_id: '', carrier_id: '', prepared_by: '', shipping_date: '',ordershipping_status_id:''})
-  const [valorsLineInicials, setValorsLineInicials] = useState({shipping_order_id: '',product_id: '', quantity: ''})
+  const [valorsInicials, setValorsInicials] = useState({ client_id: '', carrier_id: '', prepared_by: '', shipping_date: '', ordershipping_status_id: '' })
+  const [valorsLineInicials, setValorsLineInicials] = useState({ shipping_order_id: '', product_id: '', quantity: '' })
   const [clientes, setClientes] = useState([])
   const [carriers, setCarriers] = useState([])
   const [users, setUsers] = useState([])
   const [products, setProducts] = useState([])
-  const [arrayProductos,setArray] = useState([])
-  
+  const [arrayProductos, setArray] = useState([])
+
 
   useEffect(async () => {
-    axios.get(`${apiUrl}/ordershipping`, {headers: {"auth-token": localStorage.getItem("token")}})
-    .then(response => {
-      setOrder(response.data)
-    })
-    .catch(e => {
-      console.log(e)
-    }
-    )
-
-    axios.get(`${apiUrl}/client`, {headers: {"auth-token": localStorage.getItem("token")}})
-    .then(response => {
-      setClientes(response.data)
-    })
-    .catch(e => {
-      console.log(e)
-    }
-    )
-
-    axios.get(`${apiUrl}/carrier`, {headers: {"auth-token": localStorage.getItem("token")}})
-    .then(response => {
-      setCarriers(response.data)
-    })
-    .catch(e => {
-      console.log(e)
-    }
-    )
-
-    axios.get(`${apiUrl}/users`, {headers: {"auth-token": localStorage.getItem("token")}})
-    .then(response => {
-      setUsers(response.data)
-    })
-    .catch(e => {
-      console.log(e)
+    axios.get(`${apiUrl}/ordershipping`, { headers: { "auth-token": localStorage.getItem("token") } })
+      .then(response => {
+        setOrder(response.data)
+      })
+      .catch(e => {
+        console.log(e)
       }
-    )
+      )
 
-    axios.get(`${apiUrl}/product`, {headers: {"auth-token": localStorage.getItem("token")}})
-    .then(response => {
-      setProducts(response.data)
-    })
-    .catch(e => {
-      console.log(e)
+    axios.get(`${apiUrl}/client`, { headers: { "auth-token": localStorage.getItem("token") } })
+      .then(response => {
+        setClientes(response.data)
+      })
+      .catch(e => {
+        console.log(e)
       }
-    )
+      )
 
-    axios.get(`${apiUrl}/ordershipping_status`, {headers: {"auth-token": localStorage.getItem("token")}})
-    .then(response => {
-      setStatus(response.data)
-    })
-    .catch(e => {
-      console.log(e)
+    axios.get(`${apiUrl}/carrier`, { headers: { "auth-token": localStorage.getItem("token") } })
+      .then(response => {
+        setCarriers(response.data)
+      })
+      .catch(e => {
+        console.log(e)
       }
-    )
+      )
+
+    axios.get(`${apiUrl}/users`, { headers: { "auth-token": localStorage.getItem("token") } })
+      .then(response => {
+        setUsers(response.data)
+      })
+      .catch(e => {
+        console.log(e)
+      }
+      )
+
+    axios.get(`${apiUrl}/product`, { headers: { "auth-token": localStorage.getItem("token") } })
+      .then(response => {
+        setProducts(response.data)
+      })
+      .catch(e => {
+        console.log(e)
+      }
+      )
+
+    axios.get(`${apiUrl}/ordershipping_status`, { headers: { "auth-token": localStorage.getItem("token") } })
+      .then(response => {
+        setStatus(response.data)
+      })
+      .catch(e => {
+        console.log(e)
+      }
+      )
   }, [])
 
   const eliminarOrder = (id) => {
-    axios.delete(`${apiUrl}ordershipping/${id}`,{headers: {"auth-token": localStorage.getItem("token")}})
-    .then(()=> {
-      alert("Orden eliminada");
-      const newOrders = orders.filter(order => order.id !== id)
-      setOrder(newOrders)
-    })
+    axios.delete(`${apiUrl}ordershipping/${id}`, { headers: { "auth-token": localStorage.getItem("token") } })
+      .then(() => {
+        alert("Orden eliminada");
+        const newOrders = orders.filter(order => order.id !== id)
+        setOrder(newOrders)
+      })
   }
 
   const afegirProducte = (producte) => {
@@ -107,7 +107,7 @@ function OrdresEnviament() {
     setArray(array)
     console.log(arrayProductos)
   }
-  
+
   const modificarOrdre = (valors) => {
     setTipoModal('Modificar')
     console.log(valors)
@@ -117,14 +117,14 @@ function OrdresEnviament() {
 
   const clientExistent = (id) => {
     const existe = clientes.find(client => client.id === id)
-    if(existe){
+    if (existe) {
       return existe.name
     }
   }
 
   const transportistaExistente = (id) => {
     const existe = carriers.find(carrier => carrier.id === id)
-    if(existe){
+    if (existe) {
       return existe.name
     }
   }
@@ -133,86 +133,105 @@ function OrdresEnviament() {
     console.log(status)
     console.log(id)
     const existe = status.find(estat => estat.id === String(id))
-    if(existe){
+    if (existe) {
       return existe.name
     }
-  } 
+  }
 
   const canviEstatModal = () => {
-      setShowModal(!showModal)
+    setShowModal(!showModal)
   }
 
   const grabar = async (values) => {
-      if (tipoModal === "Crear") {
-        if(arrayProductos.length > 0){
-          const data = await postData(url, "OrderShipping", values);
-          const newOrderId = data.id;
+    if (tipoModal === "Crear") {
+      if (arrayProductos.length > 0) {
+        const data = await postData(url, "OrderShipping", values);
+        const newOrderId = data.id;
 
-          const linesWithOrderId = arrayProductos.map((line) => ({
-            ...line,
-            shipping_order_id: newOrderId,
-          }));
+        const linesWithOrderId = arrayProductos.map((line) => ({
+          ...line,
+          shipping_order_id: newOrderId,
+        }));
 
-          await Promise.all(
-            linesWithOrderId.map((line) =>
-              postData(url, "OrderLineShipping", line)
-            )
-          );
-        }else{
-          alert("Error, has d'afegir un Order Line")
-          return;
-        }
-      } 
-      else {
-        await updateId(url, "OrderShipping", values.id, values);
+        await Promise.all(
+          linesWithOrderId.map((line) =>
+            postData(url, "OrderLineShipping", line)
+          )
+        );
+      } else {
+        alert("Error, has d'afegir un Order Line")
+        return;
       }
-      const updatedOrders = await getData(url, "OrderShipping");
-      setOrder(updatedOrders);
+    }
+    else {
+      await updateId(url, "OrderShipping", values.id, values);
+    }
+    const updatedOrders = await getData(url, "OrderShipping");
+    setOrder(updatedOrders);
 
-      canviEstatModal();
-      setArray([]);    
+    canviEstatModal();
+    setArray([]);
   };
-  
+
   return (
     <>
-    <Header title="Ordres d'Enviament" />
-    <Filter />
-    <div>
-  <Button variant="success" onClick={() => { canviEstatModal(); setTipoModal("Crear"); }}>
-    Alta Ordre
-  </Button>
-  
-  <div className="table-responsive mt-3">
-    <table className="table table-bordered table-striped table-hover text-center">
-      <thead className="table-secondary">
-        <tr>
-          <th>ID</th>
-          <th>Client</th>
-          <th>Data Estimada</th>
-          <th>Estat</th>
-          <th>Visualitzar</th>
-          <th>Modificar</th>
-          <th>Eliminar</th>
-        </tr>
-      </thead>
-      <tbody>
-        {orders.map((valors) => (
-          <tr key={valors.id}>
-            <td>{valors.id}</td>
-            <td>{clientExistent(valors.client_id)}</td>    
-            <td>{valors.shipping_date}</td>
-            <td>{estatExistent(valors.ordershipping_status_id)}</td>
-            <td>
-              <Button
-                variant="outline-secondary"
-                onClick={() => {
-                  visualitzaOrder(valors);
-                }}
-              >
-                <i className="bi bi-eye p-2"></i>
-              </Button>
-            </td>
-            <td>
+      <Header title="Ordres d'Enviament" />
+      <Filter />
+      <div class="row d-flex mx-0 bg-secondary mt-3 rounded-top">
+        <div class="col-12 order-1 pb-2 col-md-6 order-md-0 col-xl-4 d-flex">
+          <div class="d-flex rounded border mt-2 flex-grow-1 flex-xl-grow-0">
+            <div class="form-floating bg-white">
+              <select class="form-select" id="floatingSelect" aria-label="Seleccione una opción">
+                <option selected>Tria una opció</option>
+                <option value="1">Eliminar</option>
+              </select>
+              <label for="floatingSelect">Accions en lot</label>
+            </div>
+            <button class="btn rounded-0 rounded-end-2 orange-button text-white px-2 flex-grow-1 flex-xl-grow-0" type="button"><i class="bi bi-check-circle text-white px-1"></i>Aplicar</button>
+          </div>
+        </div>
+        <div class="d-none d-xl-block col-xl-4 order-xl-1"></div>
+        <div class="col-12 order-0 col-md-6 order-md-1 col-xl-4 oder-xl-2">
+          <div class="d-flex h-100 justify-content-xl-end">
+            <button type="button" class="btn btn-dark border-white text-white mt-2 my-md-2 flex-grow-1 flex-xl-grow-0"><i class="bi bi-plus-circle text-white pe-1"></i>Crear</button>
+          </div>
+        </div>
+      </div>
+      <div className="table-responsive mt-3">
+        <table class="table table-striped text-center">
+          <thead className="table-active border-bottom border-dark-subtle">
+            <tr>
+              <th scope='col'><input class="form-check-input" type="checkbox" name="" id="" /></th>
+              <th scope='col'>ID</th>
+              <th scope='col'>Client</th>
+              <th scope='col'>Data Estimada</th>
+              <th scope='col'>Estat</th>
+              <th scope='col'>Visualitzar</th>
+              <th scope='col'>Modificar</th>
+              <th scope='col'>Eliminar</th>
+            </tr>
+          </thead>
+          <tbody>
+            {orders.map((valors) => (
+              <tr key={valors.id}>
+                <td scope="row" data-cell="Seleccionar">
+                  <input class="form-check-input" type="checkbox" name="" id=""/>
+                </td>
+                <td>{valors.id}</td>
+                <td>{clientExistent(valors.client_id)}</td>
+                <td>{valors.shipping_date}</td>
+                <td>{estatExistent(valors.ordershipping_status_id)}</td>
+                <td>
+                  <Button
+                    variant="outline-secondary"
+                    onClick={() => {
+                      visualitzaOrder(valors);
+                    }}
+                  >
+                    <i className="bi bi-eye p-2"></i>
+                  </Button>
+                </td>
+                <td>
                   <Button
                     variant="outline-success"
                     onClick={() => { modificarOrdre(valors); canviEstatModal(); }}
@@ -228,25 +247,41 @@ function OrdresEnviament() {
                     <i className='bi bi-trash p-2'></i>
                   </Button>
                 </td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  </div>
-</div>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <nav aria-label="Page navigation example" class="d-block">
+          <ul class="pagination justify-content-center">
+            <li class="page-item">
+              <a class="page-link text-light-blue" href="#" aria-label="Previous">
+                <span aria-hidden="true">&laquo;</span>
+              </a>
+            </li>
+            <li class="page-item"><a class="page-link activo-2" href="#">1</a></li>
+            <li class="page-item"><a class="page-link text-light-blue" href="#">2</a></li>
+            <li class="page-item"><a class="page-link text-light-blue" href="#">3</a></li>
+            <li class="page-item">
+              <a class="page-link text-light-blue" href="#" aria-label="Next">
+                <span aria-hidden="true">&raquo;</span>
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
 
-   <Modal show = {showModal} >
+      <Modal show={showModal} >
         <Modal.Header closeButton onHide={canviEstatModal}>
           <Modal.Title>{tipoModal} Ordre Enviament</Modal.Title>
         </Modal.Header>
 
         <Modal.Body>
-          
+
           <Formik
             initialValues={(tipoModal === 'Modificar' ? valorsInicials : {
-              client_id: '', 
-              carrier_id: '', 
-              prepared_by: '', 
+              client_id: '',
+              carrier_id: '',
+              prepared_by: '',
               shipping_date: '',
               ordershipping_status_id: 1
             })}
@@ -256,56 +291,56 @@ function OrdresEnviament() {
               grabar(values)
             }}
           >
-          {({
+            {({
               values,
               errors,
               touched,
 
               /* and other goodies */
-          }) => (
+            }) => (
               <Form>
                 <div>
-                    <Button onClick={() => canviEstatModal()} variant="secondary">Tancar</Button>
-                    
-                    <Button variant={tipoModal=== "Crear" ? "success" : "info"} type='submit'>{tipoModal}</Button>                  
+                  <Button onClick={() => canviEstatModal()} variant="secondary">Tancar</Button>
+
+                  <Button variant={tipoModal === "Crear" ? "success" : "info"} type='submit'>{tipoModal}</Button>
                 </div>
                 {/* NOM PRODUCTE */}
                 <div>
                   <label htmlFor='client_id'>Client</label>
-                  <Field as="select" name="client_id" values = {values.client_id}>
+                  <Field as="select" name="client_id" values={values.client_id}>
                     <option value="">Selecciona un client:</option>
                     {clientes.map(cliente => {
                       return <option key={cliente.id} value={cliente.id}>{cliente.name}</option>
-                    }) }
+                    })}
                   </Field>
-                    {errors.client_id && touched.client_id ? <div>{errors.client_id}</div> : null}
+                  {errors.client_id && touched.client_id ? <div>{errors.client_id}</div> : null}
                 </div>
 
                 <div>
                   <label htmlFor='carrier_id'>Transportista</label>
-                  <Field as="select" name="carrier_id" values = {values.carrier_id}>
+                  <Field as="select" name="carrier_id" values={values.carrier_id}>
                     <option value="">Selecciona un transportista:</option>
                     {carriers.map(carrier => {
                       return <option key={carrier.id} value={carrier.id}>{carrier.name}</option>
-                    }) }
+                    })}
                   </Field>
-                    {errors.carrier_id && touched.carrier_id ? <div>{errors.carrier_id}</div> : null}
+                  {errors.carrier_id && touched.carrier_id ? <div>{errors.carrier_id}</div> : null}
                 </div>
 
                 <div>
                   <label htmlFor='shipping_date'>Data estimada</label>
-                  <Field type="date" name="shipping_date" values = {values.shipping_date} />
-                    {errors.shipping_date && touched.shipping_date ? <div>{errors.shipping_date}</div> : null}
-                </div>              
+                  <Field type="date" name="shipping_date" values={values.shipping_date} />
+                  {errors.shipping_date && touched.shipping_date ? <div>{errors.shipping_date}</div> : null}
+                </div>
               </Form>
             )}
           </Formik>
 
           <Formik
             initialValues={(tipoModal === 'Modificar' ? valorsLineInicials : {
-              shipping_order_id: '', 
-              product_id: '', 
-              quantity: '' 
+              shipping_order_id: '',
+              product_id: '',
+              quantity: ''
             })}
             validationSchema={OrderLineSchema}
             onSubmit={(values, { resetForm }) => {
@@ -317,45 +352,45 @@ function OrdresEnviament() {
               resetForm(); // Limpiar el formulario después de agregar
             }}
           >
-          {({
+            {({
               values,
               errors,
               touched,
               handleSubmit
 
               /* and other goodies */
-          }) => (
+            }) => (
               <Form>
 
                 {/* NOM PRODUCTE */}
                 <div>
-                <h4>Afegeix productes a la ordre:</h4>              
-                <label htmlFor='product_id'>Producte</label>
+                  <h4>Afegeix productes a la ordre:</h4>
+                  <label htmlFor='product_id'>Producte</label>
                   <Field as="select" name="product_id">
                     <option value="">Selecciona un producte</option>
                     {products.map(product => {
                       return <option key={product.id} value={product.id}>{product.name}</option>
-                    }) }
+                    })}
                   </Field>
-                    {errors.product_id && touched.product_id ? <div>{errors.product_id}</div> : null}
+                  {errors.product_id && touched.product_id ? <div>{errors.product_id}</div> : null}
                 </div>
 
                 <div>
                   <label htmlFor='quantity'>Quantitat</label>
-                  <Field 
-                    type="text" 
+                  <Field
+                    type="text"
                     name="quantity"
                     placeholder="Quantitat del producte"
-                    value = {values.quantity}
+                    value={values.quantity}
                   >
                   </Field>
-                    {errors.quantity && touched.quantity ? <div>{errors.quantity}</div> : null}
+                  {errors.quantity && touched.quantity ? <div>{errors.quantity}</div> : null}
                 </div>
 
-                <div>              
-                <Button variant="primary" type="submit">
-                  Afegir
-                </Button>                              
+                <div>
+                  <Button variant="primary" type="submit">
+                    Afegir
+                  </Button>
                 </div>
 
               </Form>
@@ -363,8 +398,8 @@ function OrdresEnviament() {
           </Formik>
 
         </Modal.Body>
-      </Modal> 
-   </>
+      </Modal>
+    </>
   )
 }
 
