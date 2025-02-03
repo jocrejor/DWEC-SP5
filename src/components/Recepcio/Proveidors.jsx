42a7626e446c78c6ce60927d132514736f10bc45
JavierCornejoLeal <cornejolealjavier@gmail.com>
2025-02-03 18:24:10 +0100

    Correción Clases

--- src/components/Recepcio/Proveidors.jsx
+++ src/components/Recepcio/Proveidors.jsx
@@ -111,22 +111,22 @@ function Proveidors() {
       <Header title="Llistat de proveidors" />
       <Filtres />
 
-      <div class="row d-flex mx-0 bg-secondary mt-3 rounded-top">
-        <div class="col-12 order-1 pb-2 col-md-6 order-md-0 col-xl-4 d-flex">
-          <div class="d-flex rounded border mt-2 flex-grow-1 flex-xl-grow-0">
-            <div class="form-floating bg-white">
-              <select class="form-select" id="floatingSelect" aria-label="Seleccione una opción">
+      <div className="row d-flex mx-0 bg-secondary mt-3 rounded-top">
+        <div className="col-12 order-1 pb-2 col-md-6 order-md-0 col-xl-4 d-flex">
+          <div className="d-flex rounded border mt-2 flex-grow-1 flex-xl-grow-0">
+            <div className="form-floating bg-white">
+              <select className="form-select" id="floatingSelect" aria-label="Seleccione una opción">
                 <option selected>Tria una opció</option>
                 <option value="1">Eliminar</option>
               </select>
               <label for="floatingSelect">Accions en lot</label>
             </div>
-            <button class="btn rounded-0 rounded-end-2 orange-button text-white px-2 flex-grow-1 flex-xl-grow-0" type="button"><i class="bi bi-check-circle text-white px-1"></i>Aplicar</button>
+            <button className="btn rounded-0 rounded-end-2 orange-button text-white px-2 flex-grow-1 flex-xl-grow-0" type="button"><i className="bi bi-check-circle text-white px-1"></i>Aplicar</button>
           </div>
         </div>
-        <div class="d-none d-xl-block col-xl-4 order-xl-1"></div>
-        <div class="col-12 order-0 col-md-6 order-md-1 col-xl-4 oder-xl-2">
-          <div class="d-flex h-100 justify-content-xl-end">
+        <div className="d-none d-xl-block col-xl-4 order-xl-1"></div>
+        <div className="col-12 order-0 col-md-6 order-md-1 col-xl-4 oder-xl-2">
+          <div className="d-flex h-100 justify-content-xl-end">
             <Button
               className="btn btn-dark border-white text-white mt-2 my-md-2 flex-grow-1 flex-xl-grow-0"
               onClick={() => {
@@ -145,7 +145,7 @@ function Proveidors() {
                 });
               }}
             >
-              <i class="bi bi-plus-circle text-white pe-1"></i>Crear
+              <i className="bi bi-plus-circle text-white pe-1"></i>Crear
             </Button>
           </div>
         </div>
