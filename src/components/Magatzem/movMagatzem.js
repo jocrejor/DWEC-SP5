7f48d3ce9e53b5b955f4f9082d8dfae8f47d666e
jocrejor <jocrejor@gmail.com>
2025-02-03 18:31:11 +0100

    moviments magatzem

---
+++ src/components/Magatzem/movMagatzem.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+export const movMagatzem = (product,operator,quantity,origin,origin_id,storage,street,shelf,space) => {
+    // importar rulApi
+    const apiUrl = import.meta.env.VITE_API_URL;
+
+    // imoptar token
+    token = localStorage.getItem('token')
+    // crear objeto 
+    const data ={
+        "product_id":product,
+        "operator_id": operator,
+        "quantity": quantity,
+        "origin": origin,
+        "origin_id": origin_id,
+        "storage_id": storage,
+        "street_id": street,
+        "shelf_id": shelf,
+        "space_id": space 
+    }
+    
+    axios.post(apiUrl,data,{
+        headers: {
+            'Content-Type': 'multipart/form-data',
+            'auth-token' : `${token}`
+        },
+    })
+    .then((resultat) => console.log(resultat))
+    .catch((error) =>console.error(error))
+}
