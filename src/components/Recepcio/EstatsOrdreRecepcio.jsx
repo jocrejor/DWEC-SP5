34a8662fbac38feaea59eff3b3931e4b562c8f2e
LauraJorda <laujortom@alu.edu.gva.es>
2025-02-04 12:12:12 +0100

    Filtres i bootstrap

--- src/components/Recepcio/EstatsOrdreRecepcio.jsx
+++ src/components/Recepcio/EstatsOrdreRecepcio.jsx
@@ -3,8 +3,9 @@ import { Formik, Form, Field } from 'formik';
 import * as Yup from 'yup';
 import { Button, Modal, Table, Spinner } from 'react-bootstrap';
 import Header from '../Header';
-import Filtres from "../Filtres";
+import Filtres from "../FiltresLiniaRecepcio";
 import axios from 'axios';
+import '../../App.css';
 
 const apiUrl = import.meta.env.VITE_API_URL;
 
@@ -89,13 +90,43 @@ function OrderReception_Status() {
     <>
       <Header title="Llistat Estats de Ordre" />
       <Filtres />
-      <div className="container-fluid pt-3">
-        <Table striped bordered hover>
-          <thead className="table-active border-bottom border-dark-subtle text-center">
+      <div className="row d-flex mx-0 bg-secondary mt-3 rounded-top">
+        <div className="col-12 order-1 pb-2 col-md-6 order-md-0 col-xl-4 d-flex">
+          <div className="d-flex rounded border mt-2 flex-grow-1 flex-xl-grow-0">
+            <div className="form-floating bg-white">
+              <select className="form-select" id="floatingSelect" aria-label="Selecciona una opció">
+                <option value="" disabled selected>Tria una opció</option>
+                <option value="1">Eliminar</option>
+              </select>
+              <label for="floatingSelect">Accions en lot</label>
+            </div>
+            <button className="btn rounded-0 rounded-end-2 orange-button text-white px-2 flex-grow-1 flex-xl-grow-0"
+              type="button"
+              aria-label="Aplicar acció en lot">
+              <i className="bi bi-check-circle text-white px-1"></i>Aplicar</button>
+          </div>
+        </div>
+        <div className="d-none d-xl-block col-xl-4 order-xl-1"></div>
+        <div className="col-12 order-0 col-md-6 order-md-1 col-xl-4 oder-xl-2">
+          <div className="d-flex h-100 justify-content-xl-end">
+            <Button
+              className="btn btn-dark border-white text-white mt-2 my-md-2 flex-grow-1 flex-xl-grow-0"
+              onClick={() => {
+                setTipoModal('Crear'); setValorsInicials({ name: '' }); canviEstatModal();
+              }}
+              aria-label="Crear nou estat de ordre">
+              <i class="bi bi-plus-circle text-white pe-1"></i>Crear</Button>
+          </div>
+        </div>
+      </div>
+
+      <div className='container-fluid pt-3'>
+        <Table className='table table-striped border text-center m-2' aria-live="polite">
+          <thead class="table-active border-bottom border-dark-subtle">
             <tr>
-              <th>ID</th>
-              <th>Nom</th>
-              <th>Accions</th>
+              <th scope="col">ID</th>
+              <th scope="col">Nom</th>
+              <th scope="col">Accions</th>
             </tr>
           </thead>
           <tbody>
@@ -106,13 +137,15 @@ function OrderReception_Status() {
             ) : (
               ordersReceptionStatus.map((valors) => (
                 <tr key={valors.id}>
-                  <td className='text-center'>{valors.id}</td>
-                  <td className='text-center'>{valors.name}</td>
-                  <td className='text-center'>
-                    <span onClick={() => modificarEstat(valors)} style={{ cursor: "pointer" }}>
+                  <td data-cell="ID">{valors.id}</td>
+                  <td data-cell="Nom">{valors.name}</td>
+                  <td data-no-colon="true">
+                    <span onClick={() => modificarEstat(valors)} style={{ cursor: "pointer" }}
+                      aria-label="Modificar estat">
                       <i className="bi bi-pencil-square"></i>
                     </span>
-                    <span onClick={() => eliminarEstat(valors.id)} className="mx-2" style={{ cursor: "pointer" }}>
+                    <span onClick={() => eliminarEstat(valors.id)} className="mx-2" style={{ cursor: "pointer" }}
+                      aria-label="Eliminar estat">
                       <i className="bi bi-trash"></i>
                     </span>
                   </td>
@@ -122,10 +155,29 @@ function OrderReception_Status() {
           </tbody>
         </Table>
       </div>
-      
+
+
+      <nav aria-label="Paginació" class="d-block">
+        <ul class="pagination justify-content-center">
+          <li class="page-item">
+            <a class="page-link text-light-blue" href="#" aria-label="Pàgina anterior">
+              <span aria-hidden="true">&laquo;</span>
+            </a>
+          </li>
+          <li class="page-item"><a class="page-link activo-2" href="#">1</a></li>
+          <li class="page-item"><a class="page-link text-light-blue" href="#">2</a></li>
+          <li class="page-item"><a class="page-link text-light-blue" href="#">3</a></li>
+          <li class="page-item">
+            <a class="page-link text-light-blue" href="#" aria-label="Pàgina següent">
+              <span aria-hidden="true">&raquo;</span>
+            </a>
+          </li>
+        </ul>
+      </nav>
+
       <Modal show={showModal} onHide={canviEstatModal}>
         <Modal.Header closeButton>
-          <Modal.Title>{tipoModal} Estat de Línia</Modal.Title>
+          <Modal.Title>{tipoModal} Estat de Ordre</Modal.Title>
         </Modal.Header>
         <Modal.Body>
           <Formik
@@ -144,19 +196,21 @@ function OrderReception_Status() {
                     placeholder="Nom del estat de línia"
                     autoComplete="off"
                     className={`form-control ${touched.name && errors.name ? 'is-invalid' : ''}`}
+                    aria-describedby="name-error"
                   />
                   {errors.name && touched.name && (
                     <div className="invalid-feedback">{errors.name}</div>
                   )}
                 </div>
-                <div className="form-group text-right">
+                <div className="form-group text-right mt-2">
                   <Button variant="secondary" onClick={canviEstatModal}>
                     Tanca
                   </Button>
                   <Button
                     variant={tipoModal === 'Modificar' ? 'success' : 'info'}
                     type="submit"
-                    className="ml-2"
+                    className="btn orange-button text-white ms-2"
+                    aria-label={`${tipoModal} estat de ordre`}
                   >
                     {tipoModal}
                   </Button>
--- src/components/Recepcio/EstatsOrdreRecepcioLinia.jsx
+++ src/components/Recepcio/EstatsOrdreRecepcioLinia.jsx
@@ -4,7 +4,8 @@ import { Formik, Form, Field } from 'formik';
 import * as Yup from 'yup';
 import { Button, Modal, Table, Spinner } from 'react-bootstrap';
 import Header from '../Header';
-import Filtres from "../Filtres";
+import Filtres from "../FiltresLiniaRecepcio";
+import '../../App.css';
 
 const apiUrl = import.meta.env.VITE_API_URL;
 
@@ -80,17 +81,20 @@ function OrderLineReception_Status() {
       <Header title="Llistat Estats de Línia" />
       <Filtres />
 
-      <div className="row d-flex mx-0 bg-secondary mt-3 rounded-top">
+      <section className="row d-flex mx-0 bg-secondary mt-3 rounded-top">
         <div className="col-12 order-1 pb-2 col-md-6 order-md-0 col-xl-4 d-flex">
           <div className="d-flex rounded border mt-2 flex-grow-1 flex-xl-grow-0">
             <div className="form-floating bg-white">
-              <select className="form-select" id="floatingSelect" aria-label="Seleccione una opción">
-                <option selected>Tria una opció</option>
+              <select className="form-select" id="floatingSelect" aria-label="Selecciona una opció">
+                <option value="" disabled selected>Tria una opció</option>
                 <option value="1">Eliminar</option>
               </select>
               <label for="floatingSelect">Accions en lot</label>
             </div>
-            <button className="btn rounded-0 rounded-end-2 orange-button text-white px-2 flex-grow-1 flex-xl-grow-0" type="button"><i className="bi bi-check-circle text-white px-1"></i>Aplicar</button>
+            <button className="btn rounded-0 rounded-end-2 orange-button text-white px-2 flex-grow-1 flex-xl-grow-0"
+              type="button"
+              aria-label="Aplicar acció en lot">
+              <i className="bi bi-check-circle text-white px-1"></i>Aplicar</button>
           </div>
         </div>
         <div className="d-none d-xl-block col-xl-4 order-xl-1"></div>
@@ -99,15 +103,17 @@ function OrderLineReception_Status() {
             <Button
               className="btn btn-dark border-white text-white mt-2 my-md-2 flex-grow-1 flex-xl-grow-0"
               onClick={() => {
-                 setTipoModal('Crear'); setValorsInicials({ name: '' }); canviEstatModal(); }}><i class="bi bi-plus-circle text-white pe-1"></i>Nou Estat de línia</Button>
-      </div>
+                setTipoModal('Crear'); setValorsInicials({ name: '' }); canviEstatModal();
+              }} aria-label="Crear nou estat de línia">
+              <i class="bi bi-plus-circle text-white pe-1"></i>Crear</Button>
+          </div>
         </div>
-      </div>
+      </section>
 
       <div className='container-fluid pt-3'>
 
-        <Table className='table table-striped border m-2'>
-          <thead class="table-active border-bottom border-dark-subtle text-center ">
+        <Table className='table table-striped border text-center m-2' aria-live="polite">
+          <thead class="table-active border-bottom border-dark-subtle">
             <tr>
               <th scope="col">ID</th>
               <th scope="col">Nom</th>
@@ -117,19 +123,25 @@ function OrderLineReception_Status() {
           <tbody>
             {ordersLineReception.length === 0 ? (
               <tr>
-                <td colSpan="13">No hi ha estats de ordres de línia </td>
+                <td colSpan="3" role="alert">No hi ha estats de ordres de línia </td>
               </tr>
             ) : (
-            ordersLineReception.map((valors) => (
-              <tr key={valors.id}>
-                  <td className='text-center'>{valors.id}</td>
-                  <td className='text-center'>{valors.name}</td>
-                  <td className='text-center'>
-                    <span onClick={() => modificarEstatOrdre(valors)} style={{ cursor: "pointer" }}>
-                      <i className="bi bi-pencil-square"></i>
+              ordersLineReception.map((valors) => (
+                <tr key={valors.id}>
+                  <td data-cell="ID">{valors.id}</td>
+                  <td data-cell="Nom">{valors.name}</td>
+                  <td data-no-colon="true">
+                    <span onClick={() => modificarEstatOrdre(valors)}
+                      style={{ cursor: "pointer" }}
+                      aria-label={`Modificar estat de línia amb ID ${valors.id}`}>
+                      <i className="bi bi-pencil-square" aria-hidden="true"></i>
                     </span>
-                    <span onClick={() => eliminarEstatOrdre(valors.id)} className="mx-2" style={{ cursor: "pointer" }}>
-                      <i className="bi bi-trash"></i>
+                    <span
+                      onClick={() => eliminarEstatOrdre(valors.id)}
+                      className="mx-2"
+                      style={{ cursor: "pointer" }}
+                      aria-label={`Eliminar estat de línia amb ID ${valors.id}`}>
+                      <i className="bi bi-trash" aria-hidden="true"></i>
                     </span>
                   </td>
                 </tr>
@@ -137,11 +149,31 @@ function OrderLineReception_Status() {
             )}
           </tbody>
         </Table>
+
+        {/* Pagination */}
+        <nav aria-label="Paginació" class="d-block">
+          <ul class="pagination justify-content-center">
+            <li class="page-item">
+              <a class="page-link text-light-blue" href="#" aria-label="Pàgina anterior">
+                <span aria-hidden="true">&laquo;</span>
+              </a>
+            </li>
+            <li class="page-item"><a class="page-link activo-2" href="#">1</a></li>
+            <li class="page-item"><a class="page-link text-light-blue" href="#">2</a></li>
+            <li class="page-item"><a class="page-link text-light-blue" href="#">3</a></li>
+            <li class="page-item">
+              <a class="page-link text-light-blue" href="#" aria-label="Pàgina següent">
+                <span aria-hidden="true">&raquo;</span>
+              </a>
+            </li>
+          </ul>
+        </nav>
       </div>
 
-      <Modal show={showModal} onHide={canviEstatModal}>
+{/* Modal */}
+      <Modal className="text-light-blue" show={showModal} onHide={canviEstatModal}>
         <Modal.Header closeButton>
-          <Modal.Title>{tipoModal} Estat de Ordre</Modal.Title>
+          <Modal.Title>{tipoModal} Estat de Línia</Modal.Title>
         </Modal.Header>
         <Modal.Body>
           <Formik initialValues={valorsInicials} validationSchema={OrderLineReception_StatusSchema} onSubmit={handleSubmit}>
@@ -149,15 +181,25 @@ function OrderLineReception_Status() {
               <Form>
                 <div className="form-group">
                   <label htmlFor="name">Nom</label>
-                  <Field id="name" type="text" name="name" placeholder="Nom del estat" autoComplete="off"className={`form-control ${touched.name && errors.name ? 'is-invalid' : ''}`}
+                  <Field id="name"
+                  type="text"
+                  name="name"
+                  placeholder="Nom del estat"
+                  autoComplete="off"
+                  className={`form-control ${touched.name && errors.name ? 'is-invalid' : ''}`}
+                  aria-describedby="nameHelp"
                   />
                   {errors.name && touched.name && (
                     <div className="invalid-feedback">{errors.name}</div>
                   )}
                 </div>
-                <div className="form-group text-right">
+                <div className="form-group text-right mt-2">
                   <Button variant="secondary" onClick={canviEstatModal}>Tanca</Button>
-                  <Button variant={tipoModal === 'Modificar' ? 'success' : 'info'} type="submit">{tipoModal}</Button>
+                  <Button variant={tipoModal === 'Modificar' ? 'success' : 'info'}
+                    type="submit"
+                    className="btn orange-button text-white ms-2"
+                  >{tipoModal}
+                  </Button>
                 </div>
               </Form>
             )}
