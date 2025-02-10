657de617e22e3f66f17148c2ca90e537874b2036
MRoig99 <miguelangelroigancio@gmail.com>
2025-02-10 09:42:39 +0100

    pagina productes afegida

--- src/components/Recepcio/Productes.jsx
+++ src/components/Recepcio/Productes.jsx
@@ -3,6 +3,8 @@ import { Formik, Form, Field } from 'formik'
 import * as Yup from 'yup'
 import { url, postData, getData, deleteData, updateId } from '../../apiAccess/crud'
 import { Button, Modal } from 'react-bootstrap';
+import axios from 'axios';
+import Header from '../Header';
 
 const ProducteSchema = Yup.object().shape({
   name: Yup.string().min(4, 'Valor mínim de 4 caracters.').max(50, 'El valor màxim és de 60 caracters').required('Valor requerit'),
@@ -11,27 +13,80 @@ const ProducteSchema = Yup.object().shape({
   weight: Yup.number().positive('El valor ha de ser positiu').required('Valor requerit'),
   lotorserial: Yup.string().matches(/(Lot|Serial|Non)/).required('Valor requerit'),
   sku: Yup.string().matches(/^[A-Z0-9]{5,10}$/, 'El SKU ha de tindre alfanumèrics en majúscules i números (5 i 10) ').required('Valor requerit'),
-  image_url: Yup.string().url("La url ha de ser correcta")
 })
 
 function Productes() {
 
   const [products, setProducts] = useState([])
   const [showModal, setShowModal] = useState(false)
+  const [showModalUpolad, setShowModalUpload] = useState(false)
   const [tipoModal, setTipoModal]  = useState("Crear")
-  const [valorsInicials, setValorsInicials] = useState({ name: '', description: '', volume: 0, weight: 0, lotorserial: 'Non', sku: '', image_url: '' })
+  const [valorsInicials, setValorsInicials] = useState({ name: '', description: '', volume: 0, weight: 0, lotorserial: 'Non', sku: '' })
+  const [image, setImage] = useState({id:0, file: null})
+  const [messageImage, setMessageImage] = useState('');
+  const [messageError, setMessageError] = useState('');
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+  const [productsPage,setProductsPage]= useState([]);
+  const apiUrl = import.meta.env.VITE_API_URL;
+  const token = localStorage.getItem('token');
   
+  // Paginació
+  const elementsPaginacio = import.meta.env.VITE_PAGINACIO;
+  
+ 
+  // Obtindreels index. 
+  useEffect (()=>{
+    const totalPages = Math.ceil(products.length / elementsPaginacio);
+    setTotalPages(totalPages);
+    console.log(totalPages)
+  },[products])
+
+  // Función para cambiar de página
+  const paginate = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
+  // Funciones para "anterior" y "siguiente"
+  const goToPreviousPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  const goToNextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  useEffect(()=> {
+    const indexOfLastItem = currentPage * elementsPaginacio;
+    const indexOfFirstItem = indexOfLastItem - elementsPaginacio;
+    const currentItems = products.slice(indexOfFirstItem, indexOfLastItem);
+    setProductsPage(currentItems)
+  },[currentPage,products])
 
 
-  useEffect(async () => {
-    const data = await getData(url, "Product")
-    setProducts(data)
+  useEffect( () => {
+   
+   axios.get(`${apiUrl}/product` , { headers: {"auth-token" : `${token}`} })
+    .then((response) => setProducts(response.data) ) 
+    .catch((error) => {console.log(error)})
+    
   }, [])
 
+
 const eliminarProducte = (id) =>{
-  deleteData(url, "Product", id) 
-  const newproducts = products.filter(item => item.id != id)
-  setProducts(newproducts)
+  axios.delete(`${apiUrl}/product/${id}` , { headers: {"auth-token" : `${token}`} })
+    .then((response) =>{ 
+      console.log(response) 
+      const newproducts = products.filter(item => item.id != id)
+      setProducts(newproducts)
+    }) 
+    .catch((error) => {console.log(error)})
+     
+ 
 }
 
 const modificarProducte = (valors) =>{
@@ -42,28 +97,161 @@ const modificarProducte = (valors) =>{
 
 const canviEstatModal = () =>{
     setShowModal(!showModal)
+    setMessageError('')
 }
 
-const grabar = async (values)=>{
+const canviEstatModalUpload = () =>{
+  setShowModalUpload(!showModalUpolad)
+}
+
+const grabar = (values)=>{
+  setMessageError('');
   if(tipoModal==="Crear"){
-    await postData(url,'Product', values)
+    axios.post(`${apiUrl}/product` ,values,  { headers: {"auth-token" : `${token}`} })
+    .then((response) =>{ 
+      console.log(response.data.message) 
+      canviEstatModal()
+      axios.get(`${apiUrl}/product` , { headers: {"auth-token" : `${token}`} })
+           .then((response) => setProducts(response.data) ) 
+           .catch((error) => {setMessageImage(error)})
+
+    }) 
+    .catch((error) => {setMessageError(error.response.data)})
+  
   }else{
-    await updateId(url,'Product',values.id,values)
+    const id=values.id;
+    delete values.id;
+    axios.put(`${apiUrl}/product/${id}` ,values ,  { headers: {"auth-token" : `${token}`} })
+    .then((response) =>{
+       console.log(response.data.message)
+       canviEstatModal()
+         axios.get(`${apiUrl}/product` , { headers: {"auth-token" : `${token}`} })
+           .then((response) => setProducts(response.data) ) 
+           .catch((error) => {setMessageImage(error)})
+
+       }) 
+    .catch((error) => {setMessageError(error.response.data)})
+  
     }
-  const data = await getData(url, "Product")
-  await setProducts(data)
-  canviEstatModal()
+ 
+ 
+    
+
+  
 }
 
 
-  return (
-    <>
+const handleImageChange = (e) => {
+  setImage({
+    ...image,
+    [e.target.name]: e.target.files[0]
+  }); 
+}
 
-<div><h2>Llistat productes</h2></div>
-    <Button variant='success' onClick={()=>{canviEstatModal(); setTipoModal("Crear")}}>Alta Producte</Button>
-      <table className="table">
-        <thead>
+
+const updateImage = (valors) =>{
+  setImage({
+    ...image,
+    "id": valors.id
+  })
+  setMessageImage('') 
+  canviEstatModalUpload()
+}
+
+const upload =  async (e)=>{
+  e.preventDefault();
+  // valiar
+ 
+
+  if (!image.file) {
+    setMessageImage('Por favor, selecciona una imagen.');
+    return;
+  }
+
+      const formData = new FormData();
+        formData.append('image', image.file); 
+        
+        try {
+            const response = await axios.put(`${apiUrl}/product/uploadimage/${image.id}` , formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                    'auth-token' : `${token}`
+                },
+            });
+            setMessageImage(response.data.message);
+            axios.get(`${apiUrl}/product` , { headers: {"auth-token" : `${token}`} })
+              .then((response) => setProducts(response.data) ) 
+              .catch((error) => {setMessageImage(error)})
+            setTimeout(() => { 
+              canviEstatModalUpload()
+            }, 1500);
+
+        } catch (error) {
+            setMessageImage('Error al subir el archivo: ' + (error.response?.data?.message || error.message));
+        }
+
+  // fi
+ 
+}
+return (
+    <>
+<Header title="Llistat productes" />
+<div className="container-fluid">
+          <div className="row d-flex mx-0 bg-secondary mt-3 rounded-top">
+            <div className="col-12 order-1 pb-2 col-md-6 order-md-0 col-xl-4 d-flex">
+              <div className="d-flex rounded border mt-2 flex-grow-1 flex-xl-grow-0">
+                <div className="form-floating bg-white">
+                  <select
+                    className="form-select"
+                    id="floatingSelect"
+                    aria-label="Seleccione una opción"
+                  >
+                    <option>Tria una opció</option>
+                    <option value="1">Eliminar</option>
+                  </select>
+                  <label htmlFor="floatingSelect">Accions en lot</label>
+                </div>
+                <button
+                  className="btn rounded-0 rounded-end-2 orange-button text-white px-2 flex-grow-1 flex-xl-grow-0"
+                  type="button"
+                >
+                  <i className="bi bi-check-circle text-white px-1"></i>Aplicar
+                </button>
+              </div>
+            </div>
+            <div className="d-none d-xl-block col-xl-4 order-xl-1"></div>
+            <div className="col-12 order-0 col-md-6 order-md-1 col-xl-4 oder-xl-2">
+              <div className="d-flex h-100 justify-content-xl-end">
+                <button
+                  onClick={() => {
+                    canviEstatModal(); 
+                    setTipoModal("Crear");
+                  }}
+                  type="button"
+                  className="btn btn-dark border-white text-white mt-2 my-md-2 flex-grow-1 flex-xl-grow-0"
+                >
+                  <i className="bi bi-plus-circle text-white pe-1"></i>Crear
+                </button>
+                <button
+                  onClick={() => {
+                    setTabla("ListarOrder");
+                  }}
+                  type="button"
+                  className="btn btn-dark ms-2 border-white text-white mt-2 my-md-2 flex-grow-1 flex-xl-grow-0"
+                >
+                  Llistar Orders Picking
+                </button>
+              </div>
+            </div>
+          </div>
+
+
+
+
+     <table className="table table-striped text-center">
+        <thead className="table-active border-bottom border-dark-subtle">
         <tr>
+        <th scope="col"></th>
           <th scope="col">Id</th>
           <th scope="col">Nom</th>
           <th scope="col">Descripció</th>
@@ -71,30 +259,66 @@ const grabar = async (values)=>{
           <th scope="col">Pes</th>
           <th scope="col">Control</th>
           <th scope="col">SKU</th>
-          <th scope="col">Modificar</th>
-          <th>Eliminar</th>
+          <th scope="col" colSpan={3} >Accions</th>
+        
         </tr>
         </thead>
         <tbody>
-        {(products.length == 0)?
+        {(productsPage.length == 0)?
           <tr><th>No hi han articles</th></tr>
-        :products.map((valors) => {
+           
+        :productsPage.map((valors) => {
           return (
           <tr key={valors.id}>
+            <td><img src={valors.image_url}  height="50"/></td>
             <td>{valors.id}</td>
             <td>{valors.name}</td>
-            <td>{valors.description}</td>
+            <td>{valors.description.slice(0,30)}</td>
             <td>{valors.volume}</td>
             <td>{valors.weight}</td>
             <td>{valors.lotorserial}</td>
             <td>{valors.sku}</td>
-            <td><Button variant="warning"  onClick={()=> {modificarProducte(valors);canviEstatModal(); }}>Modificar</Button></td>
-            <td><Button variant="primary"  onClick={()=> {eliminarProducte(valors.id)}}>Eliminar</Button></td>
+            <td><span onClick={()=> {updateImage(valors)}}><i className="bi bi-image"></i></span></td>
+            <td><span onClick={()=> {modificarProducte(valors);canviEstatModal(); }}><i className="bi bi-pencil-square"></i></span></td>
+            <td><span  onClick={()=> {eliminarProducte(valors.id)}}> <i className="bi bi-trash"></i></span></td>
 
           </tr>)
         })}
         </tbody>
       </table>
+      
+      </div>      
+      
+      <nav aria-label="Page navigation example" className="d-block">
+          <ul className="pagination justify-content-center">
+            <li className={`page-item ${currentPage === 1 ? 'disabled' : ''}`}>
+              <a className="page-link text-light-blue" href="#" aria-label="Previous" onClick={(e) => { e.preventDefault(); goToPreviousPage(); }}>
+                <span aria-hidden="true">&laquo;</span>
+              </a>
+            </li>
+            
+            {Array.from({ length: totalPages }, (_, i) => i + 1).map((number) => (
+              <li key={number} className={`page-item ${currentPage === number ? 'active' : ''}`}>
+                <a className="page-link text-light-blue" href="#" onClick={(e) => { e.preventDefault(); paginate(number); }}>
+                  {number}
+                </a>
+              </li>
+            ))}
+            <li className={`page-item ${currentPage === totalPages ? 'disabled' : ''}`}>
+              <a className="page-link text-light-blue" href="#" aria-label="Next" onClick={(e) => {e.preventDefault(); goToNextPage(); }}>
+                <span aria-hidden="true">&raquo;</span>
+              </a>
+            </li>
+          </ul>
+        </nav>
+
+
+
+
+
+
+
+
 
       <Modal show={showModal} onHide={canviEstatModal}>
         <Modal.Header closeButton >
@@ -104,7 +328,7 @@ const grabar = async (values)=>{
         <Modal.Body>
           
       <Formik
-        initialValues= {(tipoModal==='Modificar'?valorsInicials: {name: '', description: '', volume: 0, weight: 0, lotorserial: 'Non', sku: '', image_url: '' })}
+        initialValues= {(tipoModal==='Modificar'?valorsInicials: {name: '', description: '', volume: 0, weight: 0, lotorserial: 'Non', sku: '' })}
         validationSchema={ProducteSchema}
         onSubmit={ values => { grabar(values)} }
       >
@@ -115,10 +339,12 @@ const grabar = async (values)=>{
           /* and other goodies */
         }) => (
           <Form>
-            <div>
+            <div className="form-group">
+              
               <label htmlFor='name'>Nom </label>
               <Field
                 type="text" 
+                className="form-control"
                 name="name"
                 placeholder="Nom del producte"
                 autoComplete="off"
@@ -128,10 +354,11 @@ const grabar = async (values)=>{
               {errors.name && touched.name ? <div>{errors.name}</div> : null}
             </div>
             {/*Em senc atacat*/}
-            <div>
+            <div className="form-group">
               <label htmlFor='description'>Descripció </label>
               <Field
                 as='textarea'
+                className="form-control"
                 type="text"
                 name="description"
                 placeholder="Descripció del producte"
@@ -141,10 +368,11 @@ const grabar = async (values)=>{
               {errors.description && touched.description ? <div>{errors.description}</div> : null}
             </div>
 
-            <div>
+            <div className="form-group">
               <label htmlFor='volume'>Volumen </label>
               <Field
                 type="number"
+                className="form-control"
                 name="volume"
                 step="0.001"
                 placeholder="0"
@@ -156,10 +384,11 @@ const grabar = async (values)=>{
 
 
 
-            <div>
+            <div className="form-group">
               <label htmlFor='weight'>Pes </label>
               <Field
                 type="number"
+                className="form-control"
                 name="weight"
                 step="1"
                 placeholder="0"
@@ -169,11 +398,13 @@ const grabar = async (values)=>{
               {errors.weight && touched.weight ? <div>{errors.weight}</div> : null}
             </div>
 
-            <div>
+            <div className="form-group">
               <label htmlFor='lotorserial'>Control lot o serie</label>
               <Field
                 as="select"
                 name="lotorserial"
+                className="form-control"
+                value={values.lotorserial}
               >
                 <option value="">
                   Selecciona una opció
@@ -192,32 +423,23 @@ const grabar = async (values)=>{
               {errors.lotorserial && touched.lotorserial ? <div>{errors.lotorserial}</div> : null}
             </div>
 
-            <div>
+            <div className="form-group">
               <label htmlFor='sku'>SKU </label>
               <Field
                 type="text"
+                className="form-control"
                 name="sku"
                 placeholder="sku del producte"
-                autocomplete="off"
 
                 value={values.sku}
               />
               {errors.sku && touched.sku ? <div>{errors.sku}</div> : null}
             </div>
-            <div>
-              <label htmlFor='image_url'>url de la imatge </label>
-              <Field
-                type="text"
-                name="image_url"
-                placeholder="url de la imatge del producte"
-                autoComplete="off"
-
-                value={values.image_url}
-              />
-              {errors.image_url && touched.image_url ? <div>{errors.image_url}</div> : null}
+            <div className="mb-3">
+                <p className='text-danger'>{messageError}</p>
             </div>
-            <div>
-            <Button variant="secondary" onClick={canviEstatModal}>Close</Button>
+            <div className="form-group">
+              <Button variant="secondary" onClick={canviEstatModal}>Close</Button>
 
               <Button variant={tipoModal==="Modificar"?"success":"info"} type="submit">{tipoModal}</Button>      
         
@@ -233,13 +455,52 @@ const grabar = async (values)=>{
        </Modal.Body>
       </Modal>
       
-      
-     
-  
-     
-    </>
-  )
+<Modal show={showModalUpolad} onHide={canviEstatModalUpload}>
+<Modal.Header closeButton >
+  <Modal.Title>Pujar Foto del producte</Modal.Title>
+</Modal.Header>
+
+<Modal.Body>
+    <form onSubmit={upload} >    
+    <div className="mb-3">
+      <input
+        type="hidden"
+        name="id"
+        value={image.id}
+        onChange={handleImageChange}
+      />
+    </div>
+    <div className="mb-3">
+      <label htmlFor="file" className="visually-hidden">Imagen</label>
+      <input 
+        type="file" 
+        className="form-control"
+        name="file"
+        onChange={handleImageChange}
+        />
+    </div>
+        <div className="mb-3">
+          <p className='text-danger'>{messageImage}</p>
+          </div>
+    <div>
+      <Button variant="secondary" onClick={canviEstatModalUpload}>Close</Button>
+
+      <Button variant="success" type="submit" >Enviar</Button>      
+
+    </div>
+    </form>    
+
+</Modal.Body>
+</Modal>
+
+
+
+
+
+</>
+)
 }
 
 export default Productes
+
 
