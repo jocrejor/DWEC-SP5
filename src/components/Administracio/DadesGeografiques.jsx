1f0dd313bc9931b1f92fed29c0398028f39b1fcf
jocrejor <jocrejor@gmail.com>
2025-02-14 07:24:34 +0100

    Actualitzar dades Geografiques

--- src/components/Administracio/DadesGeografiques.jsx
+++ src/components/Administracio/DadesGeografiques.jsx
@@ -1,31 +1,21 @@
- import React from 'react';
-  import Header from '../Header';
-  import {Routes,Route,Outlet } from 'react-router-dom';
-  import Pais from './Pais';
-  import Provincia from './Provincia';
-  import Ciutat from './Ciutat';
+import React from 'react';
+import Header from '../Header';
+import { Routes, Route } from 'react-router-dom';
+import City from './City';
+import Provincia from './Province';
+import State from './State';
 
-
-function DadesGeografiques() {
- 
-  
-  
-
-      return (
-          <div>
-              <Header title="Dades geografiques"></Header>
-              <Outlet />
-               <Routes >
-                        <Route path="/pais" element={<Pais />} />
-                        <Route path="/provincia/:pais" element={<Provincia />} />
-                        <Route path="/ciutat/:pais/:provincia" element={<Ciutat />} />
-                        <Route path="/" element={<Pais />} />
-                       
-               </Routes>
-               
-          </div>
-      );
-  }
-  
+const DadesGeografiques = () => {
+  return (
+    <div>
+      <Header title="Gestió de Dades Geografiques" />
+      <Routes>
+        <Route index element={<City />} />
+        <Route path="Province/:provinceId?" element={<Provincia />} />
+        <Route path="State/:stateId" element={<State />} />
+      </Routes>
+    </div>
+  );
+};
 
 export default DadesGeografiques;
