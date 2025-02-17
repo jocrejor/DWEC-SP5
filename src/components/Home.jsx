b1665d336788029321dd3636b5ddd84fc43693e9
jocrejor <jocrejor@gmail.com>
2025-02-17 09:56:56 +0100

    CharJS Gabi

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import {useState} from 'react'
 import Header from './Header'
+import { AgCharts } from 'ag-charts-react';
 
 
 function Home() {
+
+  const [chartOptions, setChartOptions] = useState({
+    // Data: Data to be displayed in the chart
+    data: [
+        { month: 'Jan', avgTemp: 2.3, iceCreamSales: 162000 },
+        { month: 'Mar', avgTemp: 6.3, iceCreamSales: 302000 },
+        { month: 'May', avgTemp: 16.2, iceCreamSales: 800000 },
+        { month: 'Jul', avgTemp: 22.8, iceCreamSales: 1254000 },
+        { month: 'Sep', avgTemp: 14.5, iceCreamSales: 950000 },
+        { month: 'Nov', avgTemp: 8.9, iceCreamSales: 200000 },
+    ],
+    // Series: Defines which chart type and data to use
+    series: [{ type: 'bar', xKey: 'month', yKey: 'iceCreamSales' }],
+});
 
   return (
     <> 
@@ -10,7 +25,8 @@ function Home() {
 
 
     <div>
-      Falta a fer per Gabi 
+      
+      <AgCharts options={chartOptions} />
     </div>
    </>
   )
