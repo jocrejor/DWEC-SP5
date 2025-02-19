4d4ac587349a1abf6371c4ffd3ca0bcefa8c4bb2
LauraJorda <laujortom@alu.edu.gva.es>
2025-02-19 09:00:09 +0100

    /

--- src/components/Magatzem/IncidenciesResoldre.jsx
+++ src/components/Magatzem/IncidenciesResoldre.jsx
@@ -178,7 +178,7 @@ function IncidenciesResoldre() {
 
         updatedData.orderline_status_id = Number(updatedData.orderline_status_id)        
 
-        axios.put(`${apiURL}incident/${id}`, updatedData, { headers: { "auth-token": token } })
+        axios.put(`${apiURL}/incident/${id}`, updatedData, { headers: { "auth-token": token } })
             .then(response => setIncident(prevIncidents =>
                 prevIncidents.map(incidents => incidents.id === updatedData.id ? response.data : incidents),
                 getDataIncident()
