5d9d4bcefd10fc323d9bbb41f0907068f373452d
LauraJorda <laujortom@alu.edu.gva.es>
2025-02-19 12:17:35 +0100

    visualitzacio d'operari (falta boto crear)

--- src/components/Recepcio/OrdresRecepcio.jsx
+++ src/components/Recepcio/OrdresRecepcio.jsx
@@ -285,7 +285,6 @@ function OrderReception() {
     }
   };
 
-
   const emmagatzemarOrdre = async (ordreId) => {
     try {
       await axios.put(`${apiUrl}/orderreception/${ordreId}`, { orderreception_status_id: 4 }, {
@@ -544,7 +543,7 @@ function OrderReception() {
       </div>
 
       <Modal show={showModal} onHide={canviEstatModal}>
-      <Modal.Header className='text-center py-4 fs-4 fw-bold m-0 text-white bg-title' closeButton>
+        <Modal.Header className='text-center py-4 fs-4 fw-bold m-0 text-white bg-title' closeButton>
           <Modal.Title>{tipoModal} Ordre de Recepció</Modal.Title>
         </Modal.Header>
         <Modal.Body>
@@ -653,7 +652,7 @@ function OrderReception() {
                               <td>{prod.name}</td>
                               <td>{prod.quantity}</td>
                               <td>
-                                <Button variant="danger" size="sm" onClick={() => eliminarProducte(index)}>
+                                <Button variant="link text-secondary p-0" onClick={() => eliminarProducte(index)}>
                                   <i className="bi bi-trash"></i>
                                 </Button>
                               </td>
@@ -666,18 +665,18 @@ function OrderReception() {
 
                   {/* BOTONS D'ACCIÓ */}
                   <Modal.Footer>
-                  <div className="form-group text-right mt-3">
-                    <Button variant="secondary" onClick={canviEstatModal}>
-                      Tanca
-                    </Button>
-                    <Button
-                      variant={tipoModal === 'Modificar' ? 'success' : 'info'}
-                      type="submit"
-                      className="btn orange-button text-white ms-2"
-                    >
-                      {tipoModal}
-                    </Button>
-                  </div>
+                    <div className="form-group text-right mt-3">
+                      <Button variant="secondary" onClick={canviEstatModal}>
+                        Tanca
+                      </Button>
+                      <Button
+                        variant={tipoModal === 'Modificar' ? 'success' : 'info'}
+                        type="submit"
+                        className="btn orange-button text-white ms-2"
+                      >
+                        {tipoModal}
+                      </Button>
+                    </div>
                   </Modal.Footer>
                 </Form>
               )}
@@ -688,7 +687,7 @@ function OrderReception() {
 
       {/* Modal per Revisar Ordre */}
       <Modal show={showReviewModal} onHide={() => setShowReviewModal(false)}>
-      <Modal.Header className='text-center py-4 fs-4 fw-bold m-0 text-white bg-title' closeButton>
+        <Modal.Header className='text-center py-4 fs-4 fw-bold m-0 text-white bg-title' closeButton>
           <Modal.Title>Ordre de Recepció</Modal.Title>
         </Modal.Header>
         <Modal.Body>
@@ -698,7 +697,9 @@ function OrderReception() {
               <p><strong>Proveïdor:</strong> {suppliers.find((sup) => sup.id === orderToReview.supplier_id)?.name}</p>
               <p><strong>Data Estimada:</strong> {formateaFecha(orderToReview.estimated_reception_date)}</p>
               <p><strong>Estat:</strong> {getStatusName(orderToReview.orderreception_status_id)}</p>
-
+              {orderToReview.orderreception_status_id >= 2 && (
+                <p><strong>Operari Associat:</strong> {users.find(user => user.id === orderToReview.created_by)?.name || 'Desconegut'}</p>
+              )}
               <h5 className="mt-3">Productes:</h5>
               <Table striped bordered hover className="table-sm">
                 <thead>
@@ -720,9 +721,6 @@ function OrderReception() {
                               <Button className="btn btn-danger mb-2" onClick={() => alert(`Incidència per ${linea.product_id}`)}>
                                 Incidència
                               </Button>
-                         {/*     <Button variant="secondary" onClick={() => alert(`Lot/Serie per ${linea.product_id}`)} className="ms-2">
-                                Lot/Serie
-                              </Button> */}
                             </>
                           )}
                         </td>
